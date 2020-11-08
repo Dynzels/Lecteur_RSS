a04c997aec460d5d11808d22818981e0fdbaacfc
Dynzels <dynzels@gmail.com>
2020-11-08 16:30:49 +0100

    Correction du traitement Ajax

--- js/app.js
+++ js/app.js
@@ -16,16 +16,29 @@ var app = {
         // GIF affichant une animation le temps du chargement
         $('#list_news').html('<img src="https://www.olybop.fr/wp-content/uploads/2018/10/loading-spin-defaut.gif">');
 
+
         $.ajax({
             url: 'news.php', // URL cible
             type: 'GET', // Le type de la requête HTTP
             data: 'url=' + rss + '&nbr=' + nbr_news, // Valeur des variables $_GET qu'on transmet
             dataType: 'html', // On désire recevoir du HTML
 
-            // Si la requête abouti
-            done: function(data) {
+            // Si la requête est un succès
+            success: function(data)
+            {
                 // On actualise le DIV avec l'ID "list_news"
                 $('#list_news').html(data);
+            },
+            // Si la requête ne se fait pas (erreur)
+            error: function(data)
+            {
+                console.log('Erreur');
+                console.log(data);
+            },
+            // Lorsque la requête a terminé
+            complete: function(data)
+            {
+                // Message de
                 console.log('Actualité affichée avec succès !');
             }
         })
--- news.php
+++ news.php
@@ -17,12 +17,11 @@ set_error_handler('exceptions_error_handler');
 
 // On récupère le flux RSS par défaut renseigné dans le fichier de config.ini
 $ini = parse_ini_file('config.ini');
-// On définit le flux à traiter (un flux par défat est traité)
+// On définit le flux à traiter (un flux par défaut est traité)
 $rss_source = htmlspecialchars(isset($_GET['url']) && !empty($_GET['url']) ? $_GET['url'] : $ini['flux_rss']);
 
 // Puis on s'assure de la conformité du flux
 try {
-
     $rss_convert = simplexml_load_file($rss_source); // Flux RSS transformé en objet PHP
     $rss = $rss_convert->channel->item; // On récupère la liste des items du flux (classés par Index -> [0] [1] etc...)
 
@@ -43,7 +42,8 @@ catch (\Exception $e) {
 ?>
 <p class=""><span>Liste des dernières news du flux suivant :</span> <?= isset($rss_convert->channel->title) ? $rss_convert->channel->title : ''; ?></p>
 <!-- Nom du flux RSS -->
-<?php for ($i=0; $i < $nbr = $nbr_news > $count ? $count : $nbr_news ; $i++) { ?>
+
+<?php for ($i=0; $i < $nbr_news ; $i++) { ?>
     <article class="card">
         <img src="<?= $rss[$i]->image->url; ?>" class="card-img-top" alt="<?= $rss[$i]->image->title; ?>">
         <div class="card-body">
